8fb710b659402081923551cfc3563493161805c1
mjarosz <jaroszmichal88@gmail.com>
2018-06-22 12:23:43 +0200

    change gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browser_sync = require('browser-sync');
+var browserSync = require('browser-sync');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('sass', function() {
-  return gulp.src('scss/**/*.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass({errLogToConsole: true, outputStyle: 'expanded'}).on('error', sass.logError))
-  .pipe(autoprefixer({browsers: ['> 1%', 'last 2 version', 'android 4']}))
-  .pipe(sourcemaps.write('./maps'))
-  .pipe(gulp.dest('css'));
+gulp.task('sass', function () {
+    gulp.src('scss/style.scss')
+        .pipe(autoprefixer({browsers: ['> 1%', 'last 2 version', 'android 4']}))
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./css'));
 });
 
-gulp.task('browser_sync', function (){
-  var files = [
-      '*.html',
-      'css/*.css'
-  ];
-  browser_sync.init(files, {
-      server: {
-          baseDir: './'
-      }
-  });
+gulp.task('browser-sync', function() {
+    browserSync.init(["*.html" ,"css/*.css", "js/*.js"], {
+        server: {
+            baseDir: "./"
+        }
+    });
 });
 
-gulp.task('watch', ['sass'], function (){
-    gulp.watch('scss/**/*.scss', ['sass']);
-});
 
-gulp.task('default', ['watch', 'browser_sync'], function() {});
+gulp.task('default', ['sass', 'browser-sync'], function () {
+    gulp.watch("scss/*.scss", ['sass']);
+});
